9eae340a93ffa6fd764d70623086c485b3752147
mohamedkhattab-17 <60518065+mohamedkhattab-17@users.noreply.github.com>
2025-08-10 15:18:39 +0200

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1754831915750

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_ad44f0e7._.js",
+  "static/chunks/src_5e179d13._.js"
+],
+    source: "dynamic"
+});
