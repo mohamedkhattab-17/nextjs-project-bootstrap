ebb05471a51a5edfa161c318ebf8e0da5fc641ae
mohamedkhattab-17 <60518065+mohamedkhattab-17@users.noreply.github.com>
2025-08-10 15:19:00 +0200

    Add .next/static/chunks/src_app_page_tsx_bdb7510e._.js 1754831936841

---
+++ .next/static/chunks/src_app_page_tsx_bdb7510e._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_ea08f47c._.js",
+  "static/chunks/src_components_9aae51ac._.js"
+],
+    source: "dynamic"
+});
